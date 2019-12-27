8e6671765dd42e73269d1549964c9ecb2f58e69b
gaoxin <gaoxin@gloriousmed.com>
2019-12-27 18:32:48 +0800

    test01

---
+++ src/test/java/com/eric/algorithm/Test01.java
@@ -0,0 +1,17 @@
+package com.eric.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Test01
+ */
+@SpringBootTest
+public class Test01 {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
