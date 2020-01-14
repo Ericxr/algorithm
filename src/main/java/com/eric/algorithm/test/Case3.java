ccd5ec197e88c3a3e66a257b158f5cd5c83107ea
gaoxin <gaoxin@gloriousmed.com>
2020-01-14 11:27:30 +0800

    push case 3

---
+++ src/main/java/com/eric/algorithm/test/Case3.java
@@ -0,0 +1,29 @@
+package com.eric.algorithm.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Case3 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    /*给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。*/
+    public static int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int index = 0, longest = 0, startIndex = 0; // 循环下标， 最大长度，起始位置
+        while (index < len) {// 当没有到末位,继续滑动
+            char c = s.charAt(index);  //获取当前的字符
+            if (map.containsKey(c)) { //如果已经存在
+                startIndex = Math.max(startIndex, map.get(c)); // 取startIndex最大值，设置为滑动的起始位置
+            }
+            longest = Math.max(longest, index - startIndex + 1); //  取最大长度
+            map.put(c, index + 1); // 存放值，  转译下标
+            index++;
+        }
+        return longest;
+    }
+}
--- src/test/java/com/eric/algorithm/Test01.java
+++ src/test/java/com/eric/algorithm/Test01.java
@@ -1,17 +1,51 @@
 package com.eric.algorithm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 /**
  * Test01
  */
 @SpringBootTest
+@Slf4j
 public class Test01 {
 
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * 示例:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
     @Test
     public void test01() {
-
+        int[] nums = {2, 7, 11, 15};
+        int target = 18;
+        int[] result = twoSum(nums, target);
+        log.info("两数之和：target: {} , index: {}", target, result);
     }
 
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] resultArr = null;
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = i;
+            Integer firstNum = nums[i];
+            int otherNum = target - firstNum;
+            if (map.containsKey(otherNum)) {
+                //有值，已经找到对应的数据
+                resultArr = new int[]{map.get(otherNum), index};
+                break;
+            } else {
+                map.put(firstNum, index);
+            }
+        }
+        return resultArr;
+    }
 }
